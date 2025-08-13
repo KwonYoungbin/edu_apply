28c48338dde1e33ba9e189ce593c60032c35ccc8
youngbin <youngbin@youngbinui-Macmini.local>
2025-08-13 18:08:20 +0900

    add gitignore

--- local_setting.py
+++ local_setting.py
@@ -1,10 +1,10 @@
 DATABASES = {
     'default' : {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'DATABASE 명', # git 
-        'USER': 'DB접속 계정명', # root
-        'PASSWORD': 'DB접속용 비밀번호', #쉿! 알지?
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'edu',
+        'USER': 'root',
+        'PASSWORD': '1234',
         'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'PORT': '5432',
     }
 }
