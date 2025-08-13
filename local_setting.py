9bb82fb659aa118153475b62a3b3ef623e901acb
youngbin <youngbin@youngbinui-Macmini.local>
2025-08-13 17:36:16 +0900

    first commit

---
+++ local_setting.py
@@ -0,0 +1,10 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DATABASE 명', # git 
+        'USER': 'DB접속 계정명', # root
+        'PASSWORD': 'DB접속용 비밀번호', #쉿! 알지?
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
