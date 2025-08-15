bef059faee542b474bb6484079a0af42e691461c
youngbin <youngbin@youngbinui-Macmini.local>
2025-08-15 21:46:01 +0900

    access token 유효 시간 변경 (30m -> 60m)

--- EduApply/settings.py
+++ EduApply/settings.py
@@ -146,7 +146,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'USER_ID_FIELD': 'id',
     'USER_ID_CLAIM': 'user_id',
