199b9f4b17dca326fd728f1418d74f852a2f0ff9
youngbin <youngbin@youngbinui-Macmini.local>
2025-08-16 17:51:50 +0900

    fix: popular 정렬시, 같은 값인 경우 생성일 기준 정렬

--- apps/common_views.py
+++ apps/common_views.py
@@ -28,7 +28,7 @@ class BaseItemListAPIView(generics.ListAPIView):
 
         sort = self.request.query_params.get('sort')
         if sort == 'popular':
-            queryset = queryset.annotate(num_registrations=Count('testregistration')).order_by('-num_registrations')
+            queryset = queryset.annotate(num_registrations=Count('testregistration')).order_by('-num_registrations', 'created_at')
         else:
             queryset = queryset.order_by('created_at')
 
