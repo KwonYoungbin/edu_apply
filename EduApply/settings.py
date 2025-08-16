d6e978734f681907faa27cfc6f0d969902345a5d
youngbin <youngbin@youngbinui-Macmini.local>
2025-08-16 15:09:52 +0900

    fix: custom user 모델 install 순서 변경

--- EduApply/settings.py
+++ EduApply/settings.py
@@ -35,6 +35,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'apps.users',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -42,7 +43,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'apps.users',
     'apps.tests',
     'apps.courses',
     'apps.test_registrations',
